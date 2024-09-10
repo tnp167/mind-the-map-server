798ec7bd184e5a377119cc3148eb72ddd5c19cb8
tnp_167 <tnp_167@outlook.com>
2024-09-10 22:08:38 +0100

    Add production to knexfile

--- knexfile.js
+++ knexfile.js
@@ -28,4 +28,14 @@ module.exports = {
       charset: "utf8",
     },
   },
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      charset: "utf8",
+    },
+  },
 };
